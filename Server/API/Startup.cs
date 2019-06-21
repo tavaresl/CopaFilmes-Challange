8cd00562854c275559825fb65d7e3fab8c0ef0f8
Lucas Tavares <lucastavaresdarocha@gmail.com>
2019-06-21 00:11:27 -0300

    fix cors

--- Server/API/Startup.cs
+++ Server/API/Startup.cs
@@ -28,6 +28,7 @@ namespace API
         {
             services.AddHttpClient<IMovieService, MovieService>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +44,7 @@ namespace API
             }
 
             app.UseHttpsRedirection();
+            app.UseCors(option => option.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
